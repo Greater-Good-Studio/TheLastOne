598cd54abf55abff97b569d08baaffa23848a7e8
Bangeunseong <bangeunseong@naver.com>
2025-08-21 20:17:38 +0900

    [feat] Initialize UI Sources

---
+++ Assets/3.Artworks/UI Sources/Shift - Complete Sci-Fi UI/Scripts/Event/HoldKeyEvent.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Michsky.UI.Shift
+{
+    public class HoldKeyEvent : MonoBehaviour
+    {
+        [Header("Key")]
+        [SerializeField]
+        public KeyCode hotkey;
+
+        [Header("Action")]
+        [SerializeField]
+        public UnityEvent holdAction;
+        [SerializeField]
+        public UnityEvent releaseAction;
+
+        bool wasHolding = false;
+
+        void Update()
+        {
+            bool nowHolding = Input.GetKey(hotkey);
+
+            if (nowHolding && !wasHolding)
+                holdAction.Invoke();
+
+            if (!nowHolding && wasHolding)
+                releaseAction.Invoke();
+
+            wasHolding = nowHolding;
+        }
+    }
+}
---
+++ Assets/3.Artworks/UI Sources/Shift - Complete Sci-Fi UI/Scripts/UI Element/UIElementSound.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using _1.Scripts.Manager.Subs;
+using _1.Scripts.Manager.Core;
+
+namespace Michsky.UI.Shift
+{
+    [ExecuteInEditMode]
+    public class UIElementSound : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler
+    {
+        public SfxType hoverSfxType = SfxType.PopupOpen;
+        public int hoverIndex = 0;
+        public SfxType clickSfxType = SfxType.ButtonClick;
+        public int clickIndex = 0;
+        
+        [Header("Settings")]
+        public bool enableHoverSound = true;
+        public bool enableClickSound = true;
+        public bool checkForInteraction = true;
+
+        private Button sourceButton;
+        private SoundManager soundManager;
+        
+        private void Start()
+        {
+            soundManager = CoreManager.Instance?.soundManager;
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if (checkForInteraction == true && sourceButton != null && sourceButton.interactable == false)
+                return;
+
+            if (enableHoverSound == true)
+            {
+                soundManager?.PlayUISFX(hoverSfxType, -1, hoverIndex);
+            }
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (checkForInteraction == true && sourceButton != null && sourceButton.interactable == false)
+                return;
+
+            if (enableClickSound == true)
+            {
+                soundManager?.PlayUISFX(clickSfxType, -1, clickIndex);
+            }
+        }
+    }
+}
